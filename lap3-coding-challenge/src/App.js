b7c0e52dea534c1380c114fb303c71b78368109b
kharileigh <kharileighmiller@gmail.com>
2022-05-11 13:24:47 +0100

    CLEAN UP

--- lap3-coding-challenge/src/App.js
+++ lap3-coding-challenge/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { NavBar } from './components';
+
+import * as Pages from './pages';
 
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/:username/:id" element={<Data />}/>
-    </Routes>
+    <div id='app'>
+      <NavBar />
+      <main>
+            <Routes>
+              <Route path="/" element={<Pages.Search />} />
+              <Route path="/:username" element={<Pages.UserInfo />}/>
+            </Routes>
+      </main>
+    </div>
   );
 }
 
