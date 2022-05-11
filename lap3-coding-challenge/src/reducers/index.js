e1c2f9979faefaf3548404eef74ca22de930a9d8
kharileigh <kharileighmiller@gmail.com>
2022-05-11 15:57:10 +0100

    ACTIONS

---
+++ lap3-coding-challenge/src/reducers/index.js
@@ -0,0 +1,25 @@
+const initState = {
+    user: {
+        login: "",
+        avatar_url: "",
+        repos_url: "",
+        followers_url: "",
+},
+   
+    loading: false,
+    error: false
+};
+
+const Reducer = (state=initState, action) => {
+    switch(action.type){
+        case 'LOADING':
+            return { ...state, username: action.payload, loading: true };
+        case 'LOAD_USER':
+            return { ...state, user: action.payload, loading: false, error: false };
+        case 'ERROR':
+            return { ...state, error: action.payload };
+        default: 
+            return state;
+    }
+}
+
