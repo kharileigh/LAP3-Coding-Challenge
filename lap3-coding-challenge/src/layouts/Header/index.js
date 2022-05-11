4456d509c5bb8181bc9d09f7293e0ca952f11509
kharileigh <kharileighmiller@gmail.com>
2022-05-11 12:41:44 +0100

    removed unnecessary react files

--- lap3-coding-challenge/src/layouts/Header/index.js
+++ lap3-coding-challenge/src/layouts/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../../images';
 
 const Header = () => { 
     return <>
         <nav>
-        <img ></img>
+        <img style={{ }} src='../../images/github-logo.png' alt='github-logo'/>
             <NavLink to='/search'>Search New User</NavLink>
         </nav>
         </>
