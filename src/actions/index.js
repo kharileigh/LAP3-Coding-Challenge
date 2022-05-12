3dbc5589aa3c4f19b0b4dc7191a1e50e1381b6b0
kharileigh <kharileighmiller@gmail.com>
2022-05-12 14:30:16 +0100

    DELETED FILES

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
 
-const loading = user => (
+
+export const loading = user => (
     {
         type: 'LOADING',
         payload: user
@@ -8,35 +8,42 @@ const loading = user => (
 );
 
 
-export const loadUser = ({ user: {  login, avatar_url, repos_url, followers_url } }) => ({
+export const loadUser = ({ user: { login, avatar_url, repos_url, followers_url } }) => ({
     type: 'LOAD_USER',
     payload: { login, avatar_url, repos_url, followers_url }
 });
 
-const fetchUser = async searchUser => {
-    try {
-        const { data } = await axios.get(`https://api.github.com/users/${searchUser}`);
-        return data[0];
-    } catch(err) {
-        if (data.status === 404) { throw Error('Ain\t no code here for you to steal!') }
-        throw new Error(err.message)
+
+export const error = err => (
+    {
+        type: 'ERROR',
+        payload: 'Research your victim properly!'
     }
-}
-
-export const getUser = searchUser => {
-    return (dispatch) => {
-        dispatch(loading(searchUser));
-        try {
-            const { login, avatar_url, repos_url, followers_url } = fetchUser(searchUser);
-            dispatch(loadUser({ user: {login, avatar_url, repos_url, followers_url}}))
-        } catch (err) {
-            if (data.status === 404) { throw Error('Research your victim properly!') }
-            dispatch({ type: 'ERROR', payload: err.message })
-        };
-    };
-};
+);
 
 
+// const fetchUser = async searchUser => {
+//     try {
+//         const { data } = await axios.get(`https://api.github.com/users/${searchUser}`);
+//         return data[0];
+//     } catch(err) {
+//         if (data.status === 404) { throw Error('Ain\t no code here for you to steal!') }
+//         throw new Error(err.message)
+// //     }
+// // }
+
+// export const getUser = searchUser => {
+//     return (dispatch) => {
+//         dispatch(loading(searchUser));
+//         try {
+//             const { login, avatar_url, repos_url, followers_url } = fetchUser(searchUser);
+//             dispatch(loadUser({ user: {login, avatar_url, repos_url, followers_url}}))
+//         } catch (err) {
+//             if (data.status === 404) { throw Error('Research your victim properly!') }
+//             dispatch({ type: 'ERROR', payload: err.message })
+//         };
+//     };
+// };
 
 
 
